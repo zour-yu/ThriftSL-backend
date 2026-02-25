7bb4098c5e809103ab473ac22fa3918063900f6e
Reshan Hirusha <vihal606@gmail.com>
2026-02-25 23:35:03 +0530

    add full item and swappable item endpoints with images using aggregation

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
 
+
+
+// Full responses with images (put ABOVE "/:id")
+router.get('/swappable/full', itemController.getSwappableItemsFull);
+router.get('/:id/full', itemController.getItemByIdFull); // This will send the full item including the image url
+
+
+//router.get('/swappable', itemController.getSwappableItems); // This will
+
 // CRUD
 router.post('/', itemController.createItem);
 router.get('/', itemController.getAllItems);
