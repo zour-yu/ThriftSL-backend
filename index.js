5526073e00ffdb1df880cf0f97a65b1acc1de33c
Reshan Hirusha <vihal606@gmail.com>
2026-02-27 17:50:30 +0530

    Mail service and package configurations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
-
+const config = require('./src/config');
 const PORT = config.port;
 
 connectDB();
