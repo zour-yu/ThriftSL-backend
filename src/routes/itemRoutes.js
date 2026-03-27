495d5802188be5f4c6fa1912fa71120f701932f1
Theonecoder <vihal606@gmail.com>
2026-03-27 15:15:43 +0530

    Update itemRoutes.js

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -8,19 +8,29 @@ const itemController = require('../controllers/itemController');
 //router.get('/swappable/full', itemController.getSwappableItemsFull);
 
 //This will send the item ID use this for swap.
-router.get('/:id/full', itemController.getItemByIdFull); // This will send the full item including the image url
+//router.get('/:id/full', itemController.getItemByIdFull); // This will send the full item including the image url
 
 
 //  Profile: items listed by user
-router.get('/user/:userId', itemController.getItemsByUserId);
+//router.get('/user/:userId', itemController.getItemsByUserId);
 
 //router.get('/swappable', itemController.getSwappableItems); // This will
 
 // CRUD
-router.post('/', itemController.createItem);
-router.get('/', itemController.getAllItems);
-router.get('/:id', itemController.getItemById);
-router.put('/:id', itemController.updateItem);
-router.delete('/:id', itemController.deleteItem);
+router.get("/search/:query", itemController.searchItems);
+
+router.post("/", itemController.createItem);
+
+router.get("/", itemController.getAllItems);
+
+router.get("/category/:category", itemController.getItemsByCategory);
+
+router.get("/user/:userId", itemController.getItemsByUser);
+
+router.get("/:id", itemController.getItemById);
+
+router.put("/:id", itemController.updateItem);
+
+router.delete("/:id", itemController.deleteItem);
 
 module.exports = router;
